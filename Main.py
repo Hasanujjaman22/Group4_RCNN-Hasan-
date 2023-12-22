5654098c3245b4b163d7fdc741500ea93dec8194
Md Hasanujjaman <hasanujjamanmd2001@gmail.com>
2023-12-22 15:56:25 +0530

    RGB To Gray Conversion

--- Main.py
+++ Main.py
@@ -1,12 +1,19 @@
+
+
+
 import cv2
+import numpy as np
+
+import os
 
 import glob
 
-vidcap = cv2.VideoCapture('C:/Users/SOMA HAZRA/Desktop/research/samples/Crowd.mp4')
+vidcap = cv2.VideoCapture('F:/RCNN/Group4_RCNN/Crowd.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-      cv2.imwrite('C:/Users/SOMA HAZRA/Desktop/research/MyProg/Resampling/ExtFrame'+ "\\frame%d.jpg" % count, image)   # save frame as JPEG file      
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+      cv2.imwrite('C:/Users/hasan/OneDrive/Desktop/Research/ExtFrame'+ "\\frame%d.jpg" % count, image)   # save frame as JPEG file      
       success,image = vidcap.read()
       print('Read a new frame: ', success)
   
@@ -14,17 +21,9 @@ while success:
   
 print ('The no. of frames',count)
 
-dstPath='C:/Users/SOMA HAZRA/Desktop/research/MyProg/Resampling/GRFrame/'
-imdir = 'C:/Users/SOMA HAZRA/Desktop/research/MyProg/Resampling/ExtFrame'
-ext = ['JPG']
-files = []
-[files.extend(glob.glob(imdir + '*.' + e)) for e in ext]
-images = [cv2.imread(file) for file in files]
-try:
-        
-        gray = cv2.cvtColor(images,cv2.COLOR_BGR2GRAY)
-      
-        cv2.imwrite(dstPath,gray)
-        print ("{} is converted".format(images))
-except:
-        print ("{} is not converted".format(images))
+
+
+
+
+
+
